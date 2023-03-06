0a4db6fff3f08c51794881cdc867cb7267daa8ec
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-06 17:48:34 -0300

    create Transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,45 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
+
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 999.999.999,99
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>06/03/20223</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 999.999.999,99
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>06/03/20223</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
